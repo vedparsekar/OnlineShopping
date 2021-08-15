25c8c35237990d8412e5fb9c6c290c8b8becea6f
vedparsekar <42473712+vedparsekar@users.noreply.github.com>
2021-08-15 14:40:34 +0530

    UI changes

---
+++ OnlineShopping/UserControl/DateTimeCalender.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DateTimeCalender.ascx.cs" Inherits="OnlineShopping.UserControl.DateTimeCalender" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajax" %>
+
+<ajax:ToolkitScriptManager ID="ToolkitScriptManager" runat="server"></ajax:ToolkitScriptManager>
+<ajax:CalendarExtender ID="CalendarExtender1" runat="server" TargetControlID="txtCalender"></ajax:CalendarExtender>
+<asp:TextBox ID="txtCalender" runat="server"></asp:TextBox>
---
+++ OnlineShopping/UserControl/DateTimeCalender.ascx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineShopping.UserControl
+{
+    public partial class DateTimeCalender : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //public DateTime SelectedDate
+        public String SelectedDate
+        {
+            get
+            {
+                //return Convert.ToDateTime(txtCalender.Text);
+                return txtCalender.Text;
+            }
+            set
+            {
+                txtCalender.Text = value.ToString();
+            }
+        }
+    }
+}
